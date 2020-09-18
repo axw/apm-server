3466f494b556a2c4ee79d979c7f3829af84bcd09
Andrew Wilkins <axw@elastic.co>
2020-09-18 14:02:44 +0800

    systemtest: test tail-based sampling

--- systemtest/sampling_test.go
+++ systemtest/sampling_test.go
@@ -20,6 +20,7 @@ package systemtest_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -28,6 +29,7 @@ import (
 	"github.com/elastic/apm-server/systemtest"
 	"github.com/elastic/apm-server/systemtest/apmservertest"
 	"github.com/elastic/apm-server/systemtest/estest"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
 )
 
 func TestKeepUnsampled(t *testing.T) {
@@ -61,4 +63,46 @@ func TestKeepUnsampled(t *testing.T) {
 			assert.Len(t, result.Hits.Hits, expectedTransactionDocs)
 		})
 	}
+}
+
+func TestTailSampling(t *testing.T) {
+	systemtest.CleanupElasticsearch(t)
+	srv := apmservertest.NewUnstartedServer(t)
+	srv.Config.Sampling = &apmservertest.SamplingConfig{
+		KeepUnsampled: false,
+		Tail: &apmservertest.TailSamplingConfig{
+			Enabled:           true,
+			DefaultSampleRate: 0.5,
+			Interval:          time.Second,
+		},
+	}
+	err := srv.Start()
+	require.NoError(t, err)
+
+	const total = 200
+	const expected = 100 // 50%
+
+	tracer := srv.Tracer()
+	for i := 0; i < total; i++ {
+		tx := tracer.StartTransaction("GET /", "tail_sampling")
+		tx.Duration = time.Second * time.Duration(i+1)
+		tx.End()
+	}
+	tracer.Flush(nil)
+
+	var result estest.SearchResult
+	_, err = systemtest.Elasticsearch.Search("apm-*").WithQuery(estest.BoolQuery{
+		Filter: []interface{}{
+			estest.TermQuery{
+				Field: "transaction.type",
+				Value: "tail_sampling",
+			},
+		},
+	}).WithSize(total).Do(context.Background(), &result,
+		estest.WithCondition(func(*esapi.Response) bool {
+			return len(result.Hits.Hits) >= expected
+		}),
+	)
+	require.NoError(t, err)
+	assert.Len(t, result.Hits.Hits, expected)
 }
