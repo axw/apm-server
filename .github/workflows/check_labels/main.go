6d41fb7be0aa46c9bca8e4785a2336b6d7d13f22
Andrew Wilkins <axw@elastic.co>
2021-09-16 17:09:13 +0800

    WI

--- .github/workflows/check_labels/main.go
+++ .github/workflows/check_labels/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,6 +15,31 @@ func main() {
 	}
 	defer f.Close()
 
-	// TODO(axw) unmarshal, check labels
-	io.Copy(os.Stdout, f)
+	type label struct {
+		Name string `json:"name"`
+	}
+	var event struct {
+		PullRequest struct {
+			Labels []label
+		} `json:"pull_request"`
+	}
+	if err := json.NewDecoder(f).Decode(&event); err != nil {
+		log.Fatal(err)
+	}
+
+	// Check for any "conflicts" and "backport-*" labels.
+	var haveBackport bool
+	for _, label := range event.PullRequest.Labels {
+		if label.Name == "conflict" {
+			fmt.Printf("::error::%s\n", "Cannot merge with 'conflict' label")
+			os.Exit(1)
+		}
+		if !haveBackport && strings.HasPrefix(label.Name, "backport-") {
+			haveBackport = true
+		}
+	}
+	if !haveBackport {
+		fmt.Printf("::error::%s\n", "Missing 'backport-*' label")
+		os.Exit(1)
+	}
 }
